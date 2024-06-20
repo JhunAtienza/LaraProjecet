fc6780ab2c31204ccd14851ee4b0b8b9a6b5d1da
jhunatienza <jhunpaulatienza@gmail.com>
2024-06-20 12:08:03 +0800

    awd

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProductController;
 
-Route::put('/register', [AuthController::class, 'register']);
+Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 
